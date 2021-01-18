9175436f1ef3fa2507187f95622b402f62324bb6
qwertyoruiop <qwertyoruiop@qwertyoruiops-MacBook-Air.local>
2021-01-18 01:46:39 +0100

    bring up usb phy

--- src/drivers/usb/synopsys_drd.c
+++ src/drivers/usb/synopsys_drd.c
@@ -26,18 +26,43 @@
  */
 #include <pongo.h>
 
-struct atc {
-    uint64_t regBase;
-};
-
 struct drd {
     uint64_t regBase;
+    uint64_t atcRegBase;
+    
     struct task* irq_task;
     struct hal_device* atc_device;
     struct hal_device* mapper;
     struct hal_device* device;
 };
 
+__unused static uint32_t drd_reg_read(struct drd* drd, uint32_t offset) {
+    return *(volatile uint32_t *)(drd->regBase + offset);
+}
+__unused static uint32_t atc_reg_read(struct drd* drd, uint32_t offset) {
+    return *(volatile uint32_t *)(drd->atcRegBase + offset);
+}
+
+__unused static void drd_reg_write(struct drd* drd, uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(drd->regBase + offset) = value;
+}
+__unused static void atc_reg_write(struct drd* drd, uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(drd->atcRegBase + offset) = value;
+}
+
+__unused static void drd_reg_and(struct drd* drd, uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(drd->regBase + offset) &= value;
+}
+__unused static void atc_reg_and(struct drd* drd, uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(drd->atcRegBase + offset) &= value;
+}
+
+__unused static void drd_reg_or(struct drd* drd, uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(drd->regBase + offset) |= value;
+}
+__unused static void atc_reg_or(struct drd* drd, uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(drd->atcRegBase + offset) |= value;
+}
 
 static void drd_irq_handle() {
     puts("drd irq");
@@ -52,10 +77,32 @@ static void drd_irq_task() {
     }
 
 }
-static int drd_service_op(struct hal_device_service* svc, struct hal_device* device, uint32_t method, void* data_in, size_t data_in_size, void* data_out, size_t *data_out_size) {
-    return -1;
+
+static void atc_enable_device(struct drd* drd, bool enable) {
+    uint32_t reg = 0;
+    if (enable) {
+        reg = atc_reg_read(drd, 0) & 0xFFFFFFF8;
+    } else {
+        spin(5 * 1000);
+        reg = (atc_reg_read(drd, 0) & 0xFFFFFFF8) | 4;
+    }
+    atc_reg_write(drd, 0, reg);
 }
-static int atc_service_op(struct hal_device_service* svc, struct hal_device* device, uint32_t method, void* data_in, size_t data_in_size, void* data_out, size_t *data_out_size) {
+static void atc_bringup(struct drd* drd) {
+    atc_reg_or(drd, 0x8, 0x3);
+    atc_reg_or(drd, 0x8, 0xc);
+    spin(10 * 1000);
+    atc_reg_and(drd, 0x4, 0xfffffff7);
+    spin(10);
+    atc_reg_and(drd, 0x4, 0xfffffffC);
+    atc_reg_or(drd, 0x4, 0x4);
+    atc_reg_or(drd, 0x1c, 0x9fffffff);
+    spin(30);
+
+    atc_enable_device(drd, true);
+}
+
+static int drd_service_op(struct hal_device_service* svc, struct hal_device* device, uint32_t method, void* data_in, size_t data_in_size, void* data_out, size_t *data_out_size) {
     return -1;
 }
 static bool register_drd(struct hal_device* device, void** context) {
@@ -78,31 +125,16 @@ static bool register_drd(struct hal_device* device, void** context) {
     hal_invoke_service_op(drd->device, "hal", HAL_DEVICE_CLOCK_GATE_ON, NULL, 0, NULL, NULL);
 
     hal_invoke_service_op(drd->mapper, "dart", DART_ENTER_BYPASS_MODE, NULL, 0, NULL, NULL);
-    drd->regBase = (uint64_t)hal_map_registers(device, 0, NULL);
-    drd->irq_task = task_create_extended(device->name, drd_irq_task, TASK_IRQ_HANDLER|TASK_PREEMPT, 0);
+    drd->regBase = (uint64_t)hal_map_registers(drd->device, 0, NULL);
+    drd->atcRegBase = (uint64_t)hal_map_registers(drd->atc_device, 0, NULL);
+    drd->irq_task = task_create_extended(drd->device->name, drd_irq_task, TASK_IRQ_HANDLER|TASK_PREEMPT, 0);
 
+    atc_bringup(drd);
+    
     task_bind_to_irq(drd->irq_task, hal_get_irqno(device, 0));
     
     *context = drd;
     return true;
-}
-
-static bool register_atc(struct hal_device* device, void** context) {
-    // Apple Type C PHY
-    
-    return false;
-}
-
-static bool atc_probe(struct hal_service* svc, struct hal_device* device, void** context) {
-    uint32_t len = 0;
-    dt_node_t* node = device->node;
-    if (node) {
-        void* val = dt_prop(node, "device_type", &len);
-        if (val && strcmp(val, "atc-phy") == 0) {
-            return register_atc(device, context);
-        }
-    }
-    return false;
 }
 
 static bool drd_probe(struct hal_service* svc, struct hal_device* device, void** context) {
@@ -121,15 +153,9 @@ static struct hal_service drd_svc = {
     .probe = drd_probe,
     .service_op = drd_service_op
 };
-static struct hal_service atc_svc = {
-    .name = "atc",
-    .probe = atc_probe,
-    .service_op = atc_service_op
-};
 
 static void drd_init(struct driver* driver) {
     hal_register_hal_service(&drd_svc);
-    hal_register_hal_service(&atc_svc);
 }
 
 REGISTER_DRIVER(drd, drd_init, NULL, 0);
