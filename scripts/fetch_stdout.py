5172c49f3a281bb6a9fc67ea0a8acc0d61585ccb
0x8ff <74850058+0x8ff@users.noreply.github.com>
2023-01-03 18:19:35 -0800

    Update copyright dates

--- scripts/fetch_stdout.py
+++ scripts/fetch_stdout.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-#  Copyright (C) 2019-2022 checkra1n team
+#  Copyright (C) 2019-2023 checkra1n team
 #  This file is part of pongoOS.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
