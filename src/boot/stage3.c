dd1bf90f0a2569b5ea3ea8a8ab8d6bba24161d98
Siguza <siguza@siguza.net>
2023-01-13 00:00:49 +0100

    Fix A9/A9X NVMe timeout

--- src/boot/stage3.c
+++ src/boot/stage3.c
@@ -25,6 +25,8 @@
  *
  */
 #include "libc_workarounds.h"
+#include <stdbool.h>
+#include <stdint.h>
 #include <pongo.h>
 
 extern _Noreturn void jump_to_image(uint64_t image, uint64_t args, uint64_t tramp);
@@ -40,23 +42,50 @@ void fuse_jump(volatile void *boot_image) __asm__("fuse_jump");
 
 extern uint8_t need_to_release_L3_SRAM;
 
+// A value large enough to encompass the iBoot TEXT section,
+// but small enough to not include the embedded firmware blobs.
+// NOTE: Independent definition exists in stage2.
+#define SAFE_TEXT_SIZE 0x7ff00
+
 void patch_bootloader(void* boot_image)
 {
     strcpy((void*)((uintptr_t)boot_image + 0x200), "Stage2 KJC Loader");
 
     // Trampoline patch
-    // Start by finding "movz x18, 0"
-    volatile uint32_t *p = boot_image;
-    while(*p != 0xd2800012) ++p;
-    // Find next ret;
-    while(*p != 0xd65f03c0) ++p;
-    // Patch it
-    p[-1] = 0xb26107f2; // orr x18, xzr, 0x180000000
-    p[0] = 0xd61f0240; // br x18
+    bool tramp_done = false;
+    for(volatile uint32_t *p = boot_image, *end = (volatile uint32_t*)((uintptr_t)boot_image + SAFE_TEXT_SIZE); p < end; ++p)
+    {
+        // Start by finding "movz x18, 0"
+        if(*p == 0xd2800012)
+        {
+            // Make it load Pongo's base address instead
+            *p = 0xb26107f2; // orr x18, xzr, 0x180000000
+            // Now find the next ret
+            for(; p < end; ++p)
+            {
+                if(*p == 0xd65f03c0)
+                {
+                    tramp_done = true;
+                    break;
+                }
+            }
+            if(!tramp_done)
+            {
+                goto fail;
+            }
+            // Patch it
+            *p = 0xd61f0240; // br x18
+            break;
+        }
+    }
+    if(!tramp_done)
+    {
+        goto fail;
+    }
 
     // Keep L3 SRAM around
     // /x 000040b900781e12000000b9000040b900001032000000b9001440b900000032001400b9:00fcffff00fcffff00fcffff00fcffff00fcffff00fcffff00fcffff00fcffff00fcffff
-    for(volatile uint32_t *p = boot_image, *end = (volatile uint32_t*)((uintptr_t)boot_image + 0x7ff00); p < end; ++p)
+    for(volatile uint32_t *p = boot_image, *end = (volatile uint32_t*)((uintptr_t)boot_image + SAFE_TEXT_SIZE); p < end; ++p)
     {
         if
         (
@@ -77,6 +106,39 @@ void patch_bootloader(void* boot_image)
         }
     }
 
+    // Cursed fix for this god forsaken bootloader.
+    // On A9 and A9X, if the code running between iBootStage1 and iBootStage2 takes more than
+    // a given amount of time (1.5s?), then iBootStage2 will fail to initialise the APCIe link.
+    // Hell knows why, but it can be fixed by resetting whatever underlying hardware there is,
+    // and iBoot already contains the code to do that, but it only uses it in the iBEC profile.
+    // So what we do here is find the function that sets the reset flag and patch it to always true.
+    // This consists of:
+    //  adr x8, 0x...
+    //  nop
+    //  {orr w9, wzr, 1 | mov w9, 1}
+    //  strb w9, [x8]
+    //  strb w0, [x8, 1]
+    //  ret
+    // We just turn the second store into "strb w9, [x8, 1]".
+    // /x 080000101f2003d5e90300320901003900050039c0035fd6:1f00009fffffffffffffffffffffffffffffffffffffffff
+    // /x 080000101f2003d5290080520901003900050039c0035fd6:1f00009fffffffffffffffffffffffffffffffffffffffff
+    for(volatile uint32_t *p = boot_image, *end = (volatile uint32_t*)((uintptr_t)boot_image + SAFE_TEXT_SIZE); p < end; ++p)
+    {
+        if
+        (
+            (p[0] & 0x9f00001f) == 0x10000008 &&
+             p[1] == 0xd503201f &&
+            (p[2] == 0x52800029 || p[2] == 0x320003e9) &&
+             p[3] == 0x39000109 &&
+             p[4] == 0x39000500 &&
+             p[5] == 0xd65f03c0
+        )
+        {
+            p[4] = 0x39000509;
+            break;
+        }
+    }
+
     iorvbar_yeet(boot_image);
     aes_keygen(boot_image);
     // Ultra dirty hack: 16K support = Reconfig Engine
@@ -85,6 +147,10 @@ void patch_bootloader(void* boot_image)
         recfg_yoink(boot_image);
     }
     fuse_jump(boot_image);
+    return;
+
+fail:;
+    __asm__ volatile("b ."); // TODO: better fail?
 }
 
 /* BSS is cleaned on _start, so we cannot rely on it. */
