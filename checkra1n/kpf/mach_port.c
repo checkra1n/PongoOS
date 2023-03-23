7ee642a0fc56eda719b438c19a222ec1e7b14e99
Siguza <siguza@siguza.net>
2023-03-23 04:40:41 +0100

    Fix task_conversion_eval for 16.4

--- checkra1n/kpf/mach_port.c
+++ checkra1n/kpf/mach_port.c
@@ -199,6 +199,296 @@ static void kpf_convert_port_to_map_patch(xnu_pf_patchset_t *xnu_text_exec_patch
     xnu_pf_maskmatch(xnu_text_exec_patchset, "convert_port_to_map", matches_variant, masks_variant, sizeof(matches_variant)/sizeof(uint64_t), false, (void*)kpf_convert_port_to_map_callback);
 }
 
+static bool found_task_conversion_eval_ldr = false;
+static bool found_task_conversion_eval_bl  = false;
+static bool found_task_conversion_eval_imm = false;
+
+static bool kpf_task_conversion_eval_callback_ldr(struct xnu_pf_patch *patch, uint32_t *opcode_stream)
+{
+    uint32_t * const orig = opcode_stream;
+    uint32_t lr1 = opcode_stream[0],
+             lr2 = opcode_stream[2];
+    // Step 2
+    // Make sure that the registers used in tbz are the ones actually
+    // loaded by ldr, and that both ldr's use the same offset.
+    if((lr1 & 0x1f) != (opcode_stream[1] & 0x1f) || (lr2 & 0x1f) != (opcode_stream[3] & 0x1f) || (lr1 & 0x3ffc00) != (lr2 & 0x3ffc00))
+    {
+        panic_at(orig, "kpf_task_conversion_eval: opcode check failed");
+    }
+    if(found_task_conversion_eval_bl || found_task_conversion_eval_imm)
+    {
+        panic_at(orig, "kpf_task_conversion_eval: found both bl/imm and ldr");
+    }
+    found_task_conversion_eval_ldr = true;
+
+    // Step 3
+    // Search backwards for the check "caller == victim".
+    // If this is the case, then XNU always allows conversion, so we patch that to always be true.
+    // Since this function can be inlined in a lot of different places, our search needs to be quite resilient.
+    // Therefore, we start by noting which registers our ldr's above load, and keep track of which registers
+    // are moved to which other registers while going backwards, since the check will almost certainly use
+    // different registers. We also search for this instruction pattern:
+    //
+    // cmp xN, xM
+    // ccmp xR, xT, {0|4}, ne   -- (optional)
+    // ubfm ...                 -- (optional)
+    // adrp ...                 -- (optional)
+    // b.{eq|ne} ...
+    //
+    // Where either the cmp or ccmp registers must correspond to ours.
+    // We simply patch the first check to always succeed.
+    uint32_t regs = (1 << ((lr1 >> 5) & 0x1f)) | (1 << ((lr2 >> 5) & 0x1f));
+    for(size_t i = 0; i < 128; ++i) // arbitrary limit
+    {
+        uint32_t op = *--opcode_stream;
+        if((op & 0xffe0fc1f) == 0xeb00001f) // cmp xN, xM
+        {
+            uint32_t n1 = opcode_stream[1],
+                     n2 = opcode_stream[2];
+            size_t idx = 2;
+            if((n2 & 0x7f800000) == 0x53000000) // ubfm
+            {
+                n2 = opcode_stream[++idx];
+            }
+            if((n2 & 0x9f000000) == 0x90000000) // adrp
+            {
+                n2 = opcode_stream[++idx];
+            }
+            if
+            (
+                // Simple case: just cmp + b.{eq|ne}
+                (((n1 & 0xff00001e) == 0x54000000) && ((regs & (1 << ((op >> 5) & 0x1f))) != 0 && (regs & (1 << ((op >> 16) & 0x1f))) != 0)) ||
+                // Complex case: cmp + ccmp + b.{eq|ne}
+                (
+                    (n1 & 0xffe0fc1b) == 0xfa401000 && (n2 & 0xff00001e) == 0x54000000 &&
+                    (
+                        ((regs & (1 << ((op >> 5) & 0x1f))) != 0 && (regs & (1 << ((op >> 16) & 0x1f))) != 0) ||
+                        ((regs & (1 << ((n1 >> 5) & 0x1f))) != 0 && (regs & (1 << ((n1 >> 16) & 0x1f))) != 0)
+                    )
+                )
+            )
+            {
+                *opcode_stream = 0xeb1f03ff; // cmp xzr, xzr
+                puts("KPF: Found task_conversion_eval");
+                return true;
+            }
+        }
+        else if((op & 0xffe0ffe0) == 0xaa0003e0) // mov xN, xM
+        {
+            uint32_t src = (op >> 16) & 0x1f,
+                     dst = op & 0x1f;
+            regs |= ((regs >> dst) & 1) << src;
+        }
+    }
+    panic_at(orig, "kpf_task_conversion_eval: failed to find cmp");
+}
+
+static bool kpf_task_conversion_eval_callback_common(uint32_t *opcode_stream, bool can_double_match)
+{
+    if(found_task_conversion_eval_ldr)
+    {
+        panic_at(opcode_stream, "kpf_task_conversion_eval: found both ldr and bl/imm");
+    }
+
+    static uint32_t *last_match = NULL;
+    for(size_t i = 0; i < 0x48; ++i)
+    {
+        uint32_t *ldr = opcode_stream - i;
+        // Already matched and patched
+        if(can_double_match && ldr == last_match)
+        {
+            return false;
+        }
+
+        // Find ldr/cmp pattern
+        if
+        (!(
+            (
+                (ldr[0] == NOP && (ldr[1] & 0xff000000) == 0x58000000) // nop + ldr
+                ||
+                ((ldr[0] & 0x9f000000) == 0x90000000 && (ldr[1] & 0xffc003e0) == (0xf9400000 | ((ldr[0] & 0x1f) << 5))) // adrp + ldr
+            )
+            &&
+            ((ldr[2] & 0xffe0ffff) == (0xeb00001f | ((ldr[1] & 0x1f) << 5))) // cmp
+        ))
+        {
+            continue;
+        }
+
+        size_t idx = 3;
+        if((ldr[idx] & 0xffe0fc1b) == 0xfa401000) // ccmp {eq|ne}
+        {
+            ++idx;
+        }
+        if((ldr[idx] & 0xff00001e) != 0x54000000) // b.{eq|ne}
+        {
+            panic_at(ldr, "kpf_task_conversion_eval: no b.{eq|ne} after cmp/ccmp?");
+        }
+
+        // Subsequent matches would fail to patch
+        if(can_double_match)
+        {
+            last_match = opcode_stream;
+        }
+        ldr[2] = 0xeb1f03ff; // cmp xzr, xzr
+
+        puts("KPF: Found task_conversion_eval");
+        return true;
+    }
+    panic_at(opcode_stream, "kpf_task_conversion_eval: failed to find ldr of kernel_task");
+}
+
+static bool kpf_task_conversion_eval_callback_bl(struct xnu_pf_patch *patch, uint32_t *opcode_stream)
+{
+    uint32_t bl1 = opcode_stream[1],
+             bl2 = opcode_stream[4];
+    // Only match if funcs are the same
+    uint32_t *f1 = opcode_stream + 1 + sxt32(bl1, 26), // uint32 takes care of << 2
+             *f2 = opcode_stream + 4 + sxt32(bl2, 26); // uint32 takes care of << 2
+    if(f1 != f2)
+    {
+        return false;
+    }
+    // Search for bitfield marker in target function. We can be quite restrictive here
+    // because if this doesn't match, then nothing will and we'll get a KPF panic.
+    // Also make sure we don't seek past the end of any function here.
+    for(size_t i = 0; i < 48; ++i)
+    {
+        uint32_t op = f1[i];
+        if(op == RET)
+        {
+            return false;
+        }
+        if(op == 0x530a2900) // ubfx w0, w8, 0xa, 1
+        {
+            found_task_conversion_eval_bl = true;
+            return kpf_task_conversion_eval_callback_common(opcode_stream, false);
+        }
+    }
+    return false;
+}
+
+static bool kpf_task_conversion_eval_callback_imm(struct xnu_pf_patch *patch, uint32_t *opcode_stream)
+{
+    found_task_conversion_eval_imm = true;
+    return kpf_task_conversion_eval_callback_common(opcode_stream, true);
+}
+
+static void kpf_task_conversion_eval_patch(xnu_pf_patchset_t *xnu_text_exec_patchset)
+{
+    // This patch is here to allow the usage of the extracted tfp0 port from userland (see https://bazad.github.io/2018/10/bypassing-platform-binary-task-threads/#the-platform-binary-mitigation)
+    // The task_conversion_eval function is often inlinded tho and because of that we need to find it across the kernel.
+    // There is this line in the functon: if ((victim->t_flags & TF_PLATFORM) && !(caller->t_flags & TF_PLATFORM)) {
+    // Which compiles to a very recognisable sequence of instructions that we match against (step 1). We do some sanity
+    // checks (step 2) and then seek backwards from there to the check for "caller == victim" and make it always true.
+    // Example from an iPhone 7 13.3:
+    //
+    // 0xfffffff00713dca4      3a2f00d0       adrp x26, sym.___stack_chk_guard
+    // 0xfffffff00713dca8      5a233b91       add x26, x26, 0xec8
+    // 0xfffffff00713dcac      392f00f0       adrp x25, 0xfffffff007724000
+    // 0xfffffff00713dcb0      f5260310       adr x21, 0xfffffff00714418c
+    // 0xfffffff00713dcb4      1f2003d5       nop
+    // 0xfffffff00713dcb8      963640f9       ldr x22, [x20, 0x68]
+    // 0xfffffff00713dcbc      08a747f9       ldr x8, [x24, 0xf48]
+    // 0xfffffff00713dcc0      9f0316eb       cmp x28, x22                    <- Step 3: find this and patch it into cmp xzr, xzr
+    // 0xfffffff00713dcc4      04115cfa       ccmp x8, x28, 4, ne
+    // 0xfffffff00713dcc8      60010054       b.eq 0xfffffff00713dcf4
+    // 0xfffffff00713dccc      df0200f1       cmp x22, 0
+    // 0xfffffff00713dcd0      041156fa       ccmp x8, x22, 4, ne
+    // 0xfffffff00713dcd4      c0060054       b.eq 0xfffffff00713ddac
+    // 0xfffffff00713dcd8      218f47f9       ldr x1, [x25, 0xf18]
+    // 0xfffffff00713dcdc      e00316aa       mov x0, x22
+    // 0xfffffff00713dce0      4e0a0194       bl 0xfffffff007180618
+    // 0xfffffff00713dce4      c8ba43b9       ldr w8, [x22, 0x3b8]            <- Step 1: find this sequence
+    // 0xfffffff00713dce8      68005036       tbz w8, 0xa, 0xfffffff00713dcf4 <- Step 2: verify that the register is the same
+    // 0xfffffff00713dcec      88bb43b9       ldr w8, [x28, 0x3b8]            <- Step 2: verify that the offset is the same
+    // 0xfffffff00713dcf0      e8055036       tbz w8, 0xa, 0xfffffff00713ddac <- Step 2: same here
+    // 0xfffffff00713dcf4      81d038d5       mrs x1, tpidr_el1
+    // 0xfffffff00713dcf8      c82e4039       ldrb w8, [x22, 0xb]
+    // 0xfffffff00713dcfc      1f890071       cmp w8, 0x22
+    // 0xfffffff00713dd00      41070054       b.ne 0xfffffff00713dde8
+    //
+    // to find this with r2 run the following cmd:
+    // /x 000040b900005036000040b900005036:0000c0ff0000f8ff0000c0ff0000f8fe
+    uint64_t matches[] = {
+        0xb9400000, // ldr x*, [x*]
+        0x36500000, // tbz w*, 0xa, *
+        0xb9400000, // ldr x*, [x*]
+        0x36500000, // tbz w*, 0xa, *
+    };
+    uint64_t masks[] = {
+        0xffc00000,
+        0xfff80000,
+        0xffc00000,
+        0xfef80000, // match both tbz or tbnz
+    };
+    xnu_pf_maskmatch(xnu_text_exec_patchset, "task_conversion_eval", matches, masks, sizeof(matches)/sizeof(uint64_t), false, (void*)kpf_task_conversion_eval_callback_ldr);
+
+    // iOS 15.7.1 made this a whole lot more annoying because the flag check was moved to its own function.
+    // Finding this in all inlined places is agony.
+    //
+    // 0xfffffff007193654      e00301aa       mov x0, xN
+    // 0xfffffff007193658      dba30094       bl task_get_platform_binary
+    // 0xfffffff00719365c      80fcff34       cbz w0, 0x...
+    // 0xfffffff007193660      e00313aa       mov x0, x{16-31}
+    // 0xfffffff007193664      d8a30094       bl task_get_platform_binary
+    // 0xfffffff007193668      20fcff35       cb(n)z w0, 0x...
+    //
+    // /x e00300aa0000009400000034e00310aa0000009400000034:ffffe0ff000000fc1f0000fffffff0ff000000fc1f0000fe
+    uint64_t matches_alt[] = {
+        0xaa0003e0, // mov x0, xN
+        0x94000000, // bl 0x{same}
+        0x34000000, // cbz w0, 0x...
+        0xaa1003e0, // mov x0, x{16-31}
+        0x94000000, // bl 0x{same}
+        0x34000000, // cb(n)z w0, 0x...
+    };
+    uint64_t masks_alt[] = {
+        0xffe0ffff,
+        0xfc000000,
+        0xff00001f,
+        0xfff0ffff,
+        0xfc000000,
+        0xfe00001f,
+    };
+    xnu_pf_maskmatch(xnu_text_exec_patchset, "task_conversion_eval", matches_alt, masks_alt, sizeof(matches_alt)/sizeof(uint64_t), false, (void*)kpf_task_conversion_eval_callback_bl);
+
+    // In addition to the above "bl" case, there are also places where the calls to task_get_platform_binary were inlined.
+    // Some kernels (tvOS and audioOS on 16.1+) only contain such matches.
+    //
+    // 0xfffffff00719f1ac      10260012       and w16, w16, 0x3ff
+    // 0xfffffff00719f1b0      1f160071       cmp w16, 5
+    // 0xfffffff00719f1b4      21050054       b.ne 0xfffffff00719f258
+    // 0xfffffff00719f1b8      100540f9       ldr x16, [x8, 8]
+    // 0xfffffff00719f1bc      1f0201eb       cmp x16, x1
+    // 0xfffffff00719f1c0      01040054       b.ne 0xfffffff00719f240
+    // 0xfffffff00719f1c4      08e54139       ldrb w8, [x8, 0x79]
+    // 0xfffffff00719f1c8      88031036       tbz w8, 2, 0xfffffff00719f238
+    //
+    // /x 002400121f14007101000054000440f91f0000eb010000540004403900001036:00fcffff1ffcffff1f0000ff00fcffff1ffce0ff1f0000ff0004c0ff0000f8fe
+    uint64_t matches_imm[] = {
+        0x12002400, // and w*, w*, 0x3ff
+        0x7100141f, // cmp w*, 5
+        0x54000001, // b.ne 0x...
+        0xf9400400, // ldr x*, [x*, 0x...]
+        0xeb00001f, // cmp x*, x*
+        0x54000001, // b.ne 0x...
+        0x39400400, // ldrb w*, [x*, 0x... & 0x1]
+        0x36100000, // tbz w*, 2, 0x...
+    };
+    uint64_t masks_imm[] = {
+        0xfffffc00,
+        0xfffffc1f,
+        0xff00001f,
+        0xfffffc00,
+        0xffe0fc1f,
+        0xff00001f,
+        0xffc00400,
+        0xfef80000,
+    };
+    xnu_pf_maskmatch(xnu_text_exec_patchset, "task_conversion_eval", matches_imm, masks_imm, sizeof(matches_imm)/sizeof(uint64_t), false, (void*)kpf_task_conversion_eval_callback_imm);
+}
+
 static void kpf_mach_port_init(xnu_pf_range_t *cstring)
 {
     const char kmap_port_string[] = "userspace has control access to a"; // iOS 14 had broken panic strings
@@ -221,6 +511,7 @@ static void kpf_mach_port_patches(xnu_pf_patchset_t *xnu_text_exec_patchset)
     {
         kpf_convert_port_to_map_patch(xnu_text_exec_patchset);
     }
+    kpf_task_conversion_eval_patch(xnu_text_exec_patchset);
 }
 
 static void kpf_mach_port_finish(struct mach_header_64 *hdr)
@@ -228,6 +519,12 @@ static void kpf_mach_port_finish(struct mach_header_64 *hdr)
     if(need_convert_port_to_map_patch && !found_convert_port_to_map)
     {
         panic("Missing patch: convert_port_to_map");
+    }
+    // TODO: Some kernels only contain one type of match, but some contain both bl and imm.
+    //       For those, we have no reliably way to detect whether we matches all locations we need.
+    if(!found_task_conversion_eval_ldr && !found_task_conversion_eval_bl && !found_task_conversion_eval_imm)
+    {
+        panic("Missing patch: task_conversion_eval");
     }
 }
 
