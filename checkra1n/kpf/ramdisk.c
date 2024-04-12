565a40f3302de559b9cc959d1e5c37a4034f26d5
Nick Chan <towinchenmi@gmail.com>
2024-04-12 12:56:55 +0800

    Fix comments in ramdisk.c

--- checkra1n/kpf/ramdisk.c
+++ checkra1n/kpf/ramdisk.c
@@ -135,7 +135,7 @@ static void kpf_ramdisk_bootprep(struct mach_header_64 *hdr, palerain_option_t p
 #else
     char BSDName[16];
     uint32_t partid = 1;
-    /* have SSV and rootful but not setup rootful */
+    /* have SSV and rootful */
     if ((palera1n_flags & (palerain_option_rootful | palerain_option_ssv)) == (palerain_option_rootful | palerain_option_ssv)) {
         dt_node_t* fstab = dt_find(gDeviceTree, "fstab");
         if (!fstab) panic("invalid devicetree: no fstab!"); /* iOS 12 and below should not have SSV */
@@ -154,7 +154,7 @@ static void kpf_ramdisk_bootprep(struct mach_header_64 *hdr, palerain_option_t p
         char* root_matching = dt_prop(chosen, "root-matching", &root_matching_len);
         if (!root_matching) panic("invalid devicetree: no prop!");
         snprintf(BSDName, 16, "%s%" PRIu32, gHasConstriants ? (xnu_platform() == PLATFORM_TVOS ? "disk2s" : "disk1s") : "disk0s1s", partid);
-
+        /* Don't root from fakefs during fakefs setup or force revert */
         if ((palera1n_flags & (palerain_option_setup_rootful | palerain_option_force_revert)) == 0)
         snprintf(root_matching, root_matching_len, 
             "<dict ID=\"0\"><key>IOProviderClass</key><string ID=\"1\">IOService</string><key>BSD Name</key><string ID=\"2\">%s</string></dict>",
